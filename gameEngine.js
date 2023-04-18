17fae5c869dc30ea3a20e04580f7a9b041e01028
Felix Lindblom <life22ku@student.ju.se>
2023-04-19 00:04:29 +0200

    0.2.1

--- gameEngine.js
+++ gameEngine.js
@@ -1,1 +1,22 @@
 console.log("I ran")
+
+const stockInRow = 4
+const stockInCol = 3
+
+function genGrid(inRow,inCol){
+    let grid = [];
+    
+    let i = 0
+    while(i < inRow){
+        let z = 0
+        while(z < inCol){
+
+            z++
+        }
+
+        i++
+    }
+}
+function gridMaker(inRow,inCol){
+
+}
